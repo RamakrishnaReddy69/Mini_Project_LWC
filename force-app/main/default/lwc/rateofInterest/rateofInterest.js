d9d8398e387469826dd2025d22c154b8da7ce306
RamakrishnaReddy <ramakrishnabodiagri@gmail.com>
2026-04-23 12:53:13 +0530

    added to rast of interest

---
+++ force-app/main/default/lwc/rateofInterest/rateofInterest.js
@@ -0,0 +1,32 @@
+import { LightningElement, api } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+export default class RateofInterest extends LightningElement {
+    @api amount;
+    @api discount;
+    @api finalAmount;
+    @api remainingAmount;
+    handlechangeamount(event){
+        this.amount= event.target.value;
+        this.result();
+    }
+    handlechangediscount(event){
+        this.discount = event.target.value;
+        this.result();
+    }
+    result(){
+        if(this.amount && this.discount){
+            this.finalAmount = this.amount - (this.amount * this.discount / 100);
+            this.remainingAmount = this.amount -this.finalAmount;
+        }
+    }
+     handleresult(){
+        this.result();
+        const event = new ShowToastEvent({
+            title: 'Result',
+            message: `Final Amount: ${this.finalAmount}, Remaining Amount: ${this.remainingAmount}`,
+            variant: 'success',
+        });
+        this.dispatchEvent(event);  
+     }
+}
